61eef321256f570a0fddaeb309aa581b719e9c74
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-01-08 09:00:00 +0500

    ✅ Task Model Created

---
+++ src/db/models/user.js
@@ -0,0 +1,123 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Task = require("./task");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    pass: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 6,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password can't contain word password!");
+        }
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email!");
+        }
+      },
+    },
+    age: {
+      type: Number,
+    },
+
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    avatar: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.pass;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() },process.env.JWT_SECRET);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, pass) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login!");
+  }
+
+  const isMatched = await bcrypt.compare(pass, user.pass);
+
+  if (!isMatched) {
+    throw new Error("Unable to login!");
+  }
+
+  return user;
+};
+
+//* Delete all the tasks when user delete their account!
+userSchema.pre("remove", async function (next) {
+  const user = this;
+
+  await Task.deleteMany({ owner: user._id });
+  next();
+});
+
+//* Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("pass")) {
+    user.pass = await bcrypt.hash(user.pass, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
