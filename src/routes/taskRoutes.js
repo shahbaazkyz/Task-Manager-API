39fd3b974f8ee6dbdf166409d620c986cf049be8
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-01-11 01:49:00 +0500

    ✅ Delete operation Added!

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -20,4 +20,39 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 
+//* Deleting task.
+router.delete("/tasks/:id", auth, async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+    if (!task) {
+      return res.status(404).send("No task found!");
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//* Getting task
+router.get("/tasks/:id", auth, async (req, res) => {
+  const _id = req.params.id;
+  try {
+    // const task = await Task.findById(_id);
+    const task = await Task.findOne({ _id, owner: req.user._id });
+    if (!task) {
+      return res.status(404).send("task not found!");
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+
+
+
+
 module.exports = router;
