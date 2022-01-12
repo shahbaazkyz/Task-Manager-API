a8d8609f198101c59dc4583541c530c6d2f3a431
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-01-13 02:49:00 +0500

    ✅ Middleware added!

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv")
+dotenv.config()
+const User = require("../../../task-manager-git/src/db/models/user");
+
+const auth = async function (req, res, next) {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate yourself!" });
+  }
+};
+
+module.exports = auth;
