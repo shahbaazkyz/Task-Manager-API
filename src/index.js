c857611fadf9ddd91feead067159cc73cdac6ca5
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-01-19 09:20:00 +0500

    ✅ More restructuring!

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ dotenv.config();
 const app = require("./app");
 const port = process.env.PORT;
 
-app.use(express.json());
-
-app.use(userRoutes);
-app.use(taskRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
